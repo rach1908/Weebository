cf003db141da3e7d01ba8c2f2149c50d6e58b831
AspITSloth <fred4262@edu.campusvejle.dk>
2019-02-28 10:02:41 +0100

    Initial commit

---
+++ WebApplication1/Models/Merchandise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Animerch.Models
+{
+    public class Merchandise
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
