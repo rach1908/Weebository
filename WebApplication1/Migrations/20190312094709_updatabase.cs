f39efc1cbafa73cdffad7bdac8d801416673efb7
Rasmus Christophersen <r.t.christophersen@gmail.com>
2019-03-12 11:55:57 +0100

    Database population and migration, and some work on html

---
+++ WebApplication1/Migrations/20190312094709_updatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Animerch.Migrations
+{
+    public partial class updatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- WebApplication1/SQL/Sample Data.sql
+++ WebApplication1/SQL/Sample Data.sql
@@ -9,7 +9,7 @@ VALUES
 INSERT INTO [Transaction] ([Price], [UserId], [MerchandiseID])
 
 VALUES
-(1200, 'addb3daa-d8cf-410f-ab1a-038221a57a23', '1'),
-(2000, 'addb3daa-d8cf-410f-ab1a-038221a57a23', '2'),
-(500, '313efbdf-a159-4ae3-b38a-5335bb6259ff', '3'),
-(700, 'd0af0ade-1aa9-4f8e-8554-5ac91b4ed2fb', '4')
+(1200, '2898cbaf-e3a9-4b50-a0cf-79e061f3dbd0', '1'),
+(2000, '2898cbaf-e3a9-4b50-a0cf-79e061f3dbd0', '2'),
+(500, '32c2e34b-79a8-4921-8e59-c066c7c736fe', '3'),
+(700, '93250b13-6783-42f6-b050-3036ce087d84', '4')
