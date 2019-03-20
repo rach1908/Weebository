f581093355f290b4d185ae9befea2481ee8c40ad
Rasmus Christophersen <r.t.christophersen@gmail.com>
2019-03-20 14:57:05 +0100

    Changes to the header, isuserloggedin verification to users merch page and userpage, and also the header

--- WebApplication1/Controllers/UserpageController.cs
+++ WebApplication1/Controllers/UserpageController.cs
@@ -9,26 +9,41 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
+
 namespace Animerch.Controllers
 {    
     public class UserpageController : Controller
     {
+        public SignInManager<User> signInManager;
+        public UserManager<User> userManager;
         public List<Merchandise> Merchandises { get; private set; }
         private readonly ApplicationDbContext context;
-        public UserpageController(ApplicationDbContext context)
+        public UserpageController(ApplicationDbContext context, SignInManager<User> signInManager)
         {
+            this.signInManager = signInManager;
             this.context = context;
         }
 
         public IActionResult Index()
         {
+            if (signInManager.IsSignedIn(User))
+            {
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
                      
-            return View();
         }
 
         public IActionResult Merchandise()
         {
             var userID = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            
+            
 
             var userMerchandiseList = (from merch in context.Merchandise
                            join transaction in context.Transaction on merch.ID equals transaction.Merchandise.ID
