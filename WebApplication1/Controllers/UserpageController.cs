6d1bab569fac8f10463460ee871a889459c26102
Rasmus Christophersen <r.t.christophersen@gmail.com>
2019-03-26 11:53:24 +0100

    Crud fixing and tweaking, Edit is not working. The Transactions controller and view folder are both to be deleted at a later time

--- WebApplication1/Controllers/UserpageController.cs
+++ WebApplication1/Controllers/UserpageController.cs
@@ -43,13 +43,6 @@ namespace Animerch.Controllers
         {
             var userID = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            //var userMerchandiseList = (from merch in context.Merchandise
-            //               join transaction in context.Transaction on merch.ID equals transaction.Merchandise.ID
-            //               join user in context.User on transaction.User.Id equals user.Id
-            //               where user.Id == userID
-            //               select merch).ToList();
-
-
             var userMerchandiseList = (from merch in context.Merchandise
                                        join transaction in context.Transaction on merch.ID equals transaction.Merchandise.ID
                                        join user in context.User on transaction.User.Id equals user.Id
@@ -70,12 +63,84 @@ namespace Animerch.Controllers
             return View(userMerchandiseList);
         }
 
-        
+        // GET: Userpage/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await context.Transaction.FindAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+
+            var userMerchandiseFull = (from merch in context.Merchandise
+                                       join transactions in context.Transaction on merch.ID equals transaction.Merchandise.ID
+                                       where transactions.ID == id
+                                       select new MerchandiseFull
+                                       {
+                                           ID = transaction.ID,
+                                           Name = merch.Name,
+                                           Type = merch.Type,
+                                           Series = merch.Series,
+                                           Manufacturer = merch.Manufacturer,
+                                           Amount = transaction.Amount,
+                                           Price = transaction.Price
+                                       });
+
+            ViewData.Add("mFull", userMerchandiseFull);
+
+            return View(userMerchandiseFull as MerchandiseFull);
+        }
+
+        // POST: Userpage/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Price,Amount")] MerchandiseFull transaction)
+        {
+            if (id != transaction.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(transaction);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionExists(transaction.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
 
 
         public IActionResult Friends()
         {
             return View();
+        }
+
+        private bool TransactionExists(int id)
+        {
+            return context.Transaction.Any(e => e.ID == id);
         }
     }
 
