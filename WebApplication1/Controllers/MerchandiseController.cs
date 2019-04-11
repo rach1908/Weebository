2d28a08da78b90b6c8df71487b1322a460702e60
Rasmus Christophersen <r.t.christophersen@gmail.com>
2019-04-11 11:35:01 +0200

    Merge stuff

--- WebApplication1/Controllers/MerchandiseController.cs
+++ WebApplication1/Controllers/MerchandiseController.cs
@@ -30,44 +30,7 @@ namespace Animerch.Controllers
             return View(merchandiseList);
         }
 
-<<<<<<< HEAD
-        
-=======
-        public IActionResult Create(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var merch = context.Merchandise.Find(id);
-
-            if (merch == null)
-            {
-                return NotFound();
-            }
-
-            var merchItem = context.Merchandise.Where(x => x.ID == id).FirstOrDefault();
-
-            ViewData.Add("selectedMerch", merchItem);
-
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create([Bind("Price, Amount, MerchandiseId")] Transaction transaction)
-        {
-            transaction.UserId = (await signInManager.UserManager.GetUserAsync(signInManager.Context.User)).Id;
-
-            if (ModelState.IsValid)
-            {
-                context.Add(transaction);
-                await context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return RedirectToAction("Create/" + transaction.MerchandiseId);
-        }
->>>>>>> 6616d634326ca89b318f34e8302364436f4390b7
 
         [HttpPost]
         public async Task<IActionResult> MerchandiseAddEntry([Bind("Price,Amount,ID,User,Merchandise")]Transaction transaction)
